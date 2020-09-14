744e38fb145fdef7d01988c3c9967cb1401b463e
zhudiwei <zhudiwei_13zhfx@163.com>
2020-09-14 18:19:03 +0800

    int

---
+++ src/main/java/com/kynnnn/shardingproxy/ShardingProxyApplication.java
@@ -0,0 +1,13 @@
+package com.kynnnn.shardingproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingProxyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/kynnnn/shardingproxy/model/UserDetail.java
@@ -0,0 +1,18 @@
+package com.kynnnn.shardingproxy.model;
+
+import lombok.Data;
+
+/**
+ * @author Zhudiwei
+ * @date 2020/09/14
+ **/
+@Data
+public class UserDetail {
+
+    private Long id;
+
+    private String username;
+
+    private Integer userAge;
+
+}
